e9f1c8359152b1acefb9e7004c30a1c2b7370b4a
Shad Downey <shad@1stdibs.com>
2017-04-24 11:31:28 -0400

    update to directions

--- InterviewQuestion/ViewController.swift
+++ InterviewQuestion/ViewController.swift
@@ -15,7 +15,7 @@ import UIKit
  
  Each button should have an "on" and "off" state, signified by a background color.
  
- Tapping a button should toggle its state, AND its immediate neighbors' states.
+ Tapping a button should toggle its state, AND its immediate neighbors' states. (non-diagonal immediate neighbors)
  
  When the randomize button is tapped, the states of all of the buttons should be randomized.
 
