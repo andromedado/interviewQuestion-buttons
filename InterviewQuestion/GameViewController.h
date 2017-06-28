cf669d8ba92485a46c1b39a20574eb0496d3516e
Shad Downey <shad@1stdibs.com>
2017-06-28 09:48:06 -0400

    adding in an objective-c vc for use with the swift unfamiliar

---
+++ InterviewQuestion/GameViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameViewController.h
+//  InterviewQuestion
+//
+//  Created by Shad Downey on 6/28/17.
+//  Copyright © 2017 Shad Downey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
